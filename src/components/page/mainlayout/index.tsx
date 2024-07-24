97d715dedc2bf081bd6a3450e73f54c4e7db74a0
Samuel Oluwatobi Oluwasegun <107188941+Oluwaseg@users.noreply.github.com>
2024-07-24 21:22:15 +0100

    testing

--- src/components/page/mainlayout/index.tsx
+++ src/components/page/mainlayout/index.tsx
@@ -9,6 +9,7 @@ import youtube from '../../../../public/assets/youtube2.svg';
 import linkedin from '../../../../public/assets/linkedin2.svg';
 
 interface MainLayoutProps {
+  profilePicture?: string;
   links: { platform: string; url: string }[];
 }
 
@@ -18,7 +19,7 @@ const platformImages: Record<string, string> = {
   YouTube: youtube,
 };
 
-const MainLayout: FC<MainLayoutProps> = ({ links = [] }) => {
+const MainLayout: FC<MainLayoutProps> = ({ profilePicture, links = [] }) => {
   // Filter out links where the platform is not selected
   const validLinks = links.filter(
     (link) => link.platform && platformImages[link.platform]
@@ -44,9 +45,20 @@ const MainLayout: FC<MainLayoutProps> = ({ links = [] }) => {
         <div>
           <div className="rounded-full w-[7rem] h-[7rem] content-none bg-dark mx-[5rem] top-[4rem] absolute left-0" />
           <div className="w-[10rem] top-[12rem] mx-[3.6rem] absolute h-5 rounded-full bg-dark" />
+
           <div className="w-[5rem] top-[14rem] mx-[6.4rem] absolute h-2 rounded-full bg-dark" />
         </div>
+
         <div className="absolute flex gap-3 flex-col top-[16.5rem] left-0 mx-9">
+          {profilePicture ? (
+            <Image
+              src={profilePicture}
+              alt="Profile Picture"
+              className="w-[7rem] h-[7rem] rounded-full object-cover"
+              width={100}
+              height={100}
+            />
+          ) : null}
           {boxes.map((_, index) => (
             <div
               key={index}
